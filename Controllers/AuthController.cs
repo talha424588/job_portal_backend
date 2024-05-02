5d70e5c3c528cab3d55d28b413758d6d31797aaf
Muhammad Asad <asadghafoor26.figma@gmail.com>
2024-05-02 12:40:11 +0500

    Add project files.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobPortal.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpGet]
+        public string test()
+        {
+            return ("testing api hit");
+        }
+
+        [HttpPost]
+        
+    }
+}
